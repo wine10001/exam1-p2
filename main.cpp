ddfe4e5a87c1296339225118ea12e9bfce8e7a28
wine10001 <128204373+wine10001@users.noreply.github.com>
2023-04-17 13:48:55 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ int sc_main(int argc, char *argv[]) {
   
   //Create modules and signals
   stim testbench("testbench");
-  adder dut("dut", 1);
+  adder filter("filter", 1);
   sc_clock clk("clk", CLOCK_PERIOD, SC_NS);
   sc_signal<bool> rst("rst");
 
@@ -17,14 +17,14 @@ int sc_main(int argc, char *argv[]) {
   //Connect FIFO channels with modules
   testbench.i_clk(clk);
   testbench.o_rst(rst);
-  dut.i_clk(clk);
-  dut.i_rst(rst);
+  filter.i_clk(clk);
+  filter.i_rst(rst);
   testbench.x(x);
   testbench.y(y);
   testbench.i_result(result);
-  dut.i_n_port(x);
-  dut.y_n_port(y);
-  dut.o_result(result);
+  filter.i_n_port(x);
+  filter.y_n_port(y);
+  filter.o_result(result);
 
   sc_start(64, SC_NS);
   return 0;
